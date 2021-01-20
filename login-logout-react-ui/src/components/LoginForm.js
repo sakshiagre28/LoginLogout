66b82bd95ac2aff04b0f12faa122bf3ccc16ba09
Sakshi Agre <sakshiagre90@gmail.com>
2021-01-20 20:49:12 +0530

    LoginLogout

--- login-logout-react-ui/src/components/LoginForm.js
+++ login-logout-react-ui/src/components/LoginForm.js
@@ -1,31 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../index.css'
 class LoginForm extends React.Component
 {
     constructor(props)
     {
         super();
-        this.loginUser = this.loginUser.bind(this);
+        this.loginUser = this.loginUser.bind(this)
     }
 
     loginUser()
-    {
+    {   
+        
         alert("Login successful")
     }
     render(){
+        const headerStyle = {
+            //color: "white",
+            //backgroundColor: "DodgerBlue",
+            padding: "10px",
+            marginLeft : "10px",
+            fontFamily: "Arial"
+          }
+
         return(
         <div>
-            <div>
-            Login Form<br/>
-            <form>
-            Username : <input type ="text" name ="txtUsername"/><br/>
-            Password : <input type ="password" name  = "txtPassword"/><br/>
-            <button onClick = {this.loginUser}>Login</button>
-            </form>
+            <h2 class="h2" style = {headerStyle}>Login</h2>
+            <form className ="form-group">
+            
+            <div class ="col-md-6">
+                <div class = "p-2">
+                Username : <input type ="text" name ="txtUsername" className="form-control"/>
+                </div>
             </div>
+            <div class ="col-md-6">
+                <div class = "p-2">
+                Password : <input type ="password" name  = "txtPassword" className="form-control"/>
+                </div>
+            </div>
+            <div class ="col-md-6">
+                <div class = "p-2">
+                <Button variant="primary" onClick = {this.loginUser}>Login</Button>
+                </div>
+            </div>
+            
+            </form>
+
         </div>
     )
         }
 }
 
 export default LoginForm;
+
+/*
+
+style="text-align: center; padding-top: 35px; padding-bottom: 30px; color: white;"
+*/
